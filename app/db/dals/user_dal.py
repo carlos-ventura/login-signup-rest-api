d6617d5cdd8c19a1cbb455ba247e25a5e78e50de
carlos-ventura <carlospinho_pt@hotmail.com>
2023-02-11 17:22:50 +0100

    Create functions to check if username or email exist already

--- app/db/dals/user_dal.py
+++ app/db/dals/user_dal.py
@@ -1,5 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
 from app.db.models.user import User
+from app.models import User as InputUser
 from app.models import UserInDB
 
 
@@ -16,4 +19,14 @@ class UserDAL():
         )
         self.db_session.add(user_to_add)
         await self.db_session.commit()
+
+    async def verify_username(self, user: InputUser):
+        username_entries = await self.db_session.execute(select(User.username))
+        username_list = username_entries.scalars().all()
+        return user.username in username_list
+
+    async def verify_email(self, user: InputUser):
+        email_entries = await self.db_session.execute(select(User.email))
+        email_list = email_entries.scalars().all()
+        return user.email in email_list
 
