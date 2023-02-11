dc45b9731f587d8c28555c9e420e35656b4a77f2
carlos-ventura <carlospinho_pt@hotmail.com>
2023-02-11 17:32:27 +0100

    Aditional database setup on main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.db.config import Base, async_session, engine
 app = FastAPI()
+
+
+async def get_session():
+    try:
+        session = async_session()
+        yield session
+    finally:
+        await session.close()
+
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 @app.post("/signup")
