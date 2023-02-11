8c5a68626e85392b3a62868fa1a5f8916990f831
carlos-ventura <carlospinho_pt@hotmail.com>
2023-02-11 17:14:46 +0100

    Add custom validator to the password

--- app/models.py
+++ app/models.py
@@ -1,10 +1,24 @@
+import re
+
 from pydantic import BaseModel, EmailStr, validator
+
+
 class User(BaseModel):
     username: str
     fullname: str
     email: EmailStr
     password: str
 
+    @validator('password')
+    def password_validator(cls, password):
+        if not re.search(
+            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$%^&\*])[a-zA-Z\d!@#\$%^&\*\?]{8,}$",
+                password):
+            raise ValueError("Password must be at least 8 characters long, "
+                             "contain at least one lowercase letter, one uppercase letter, "
+                             "one number, and one special character (!, @ ,  # , $, %, ^, &, *)"
+                             )
+        return password
 
 
 class UserInDB(BaseModel):
