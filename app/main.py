e2cf7c037c8d9b891d4e486e3fb6b7bf9310da52
carlos-ventura <carlospinho_pt@hotmail.com>
2023-02-11 17:34:49 +0100

    Complete signup endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
+from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.config import Base, async_session, engine
+from app.tasks import verify_signup_task, signup_task
 app = FastAPI()
 
 
@@ -20,8 +22,19 @@ async def startup():
 
 
 @app.post("/signup")
-async def signup():
-    return
+async def signup(user: User, session: AsyncSession = Depends(get_session)):
+    existent_username, existent_email = await verify_signup_task(user, session)
+    if existent_username:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Username already in use."
+        )
+    if existent_email:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email already registered."
+        )
+    await signup_task(user, session)
 
 
 @app.post("/login")
