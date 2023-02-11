9dd0abef9eefb4d0fe85f13870b017ace1bdca80
carlos-ventura <carlospinho_pt@hotmail.com>
2023-02-11 17:22:06 +0100

    Create function to add user to the database

--- app/db/dals/user_dal.py
+++ app/db/dals/user_dal.py
@@ -1,6 +1,19 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.models.user import User
+from app.models import UserInDB
 
 
 class UserDAL():
     def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
+
+    async def add_user(self, user: UserInDB):
+        user_to_add = User(
+            username=user.username,
+            fullname=user.fullname,
+            email=user.email,
+            hashed_password=user.hashed_password
+        )
+        self.db_session.add(user_to_add)
+        await self.db_session.commit()
+
