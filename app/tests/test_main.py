93d907a1f0a4d9d266f1e1691fb74f2233b731df
carlos-ventura <carlospinho_pt@hotmail.com>
2023-02-13 11:15:18 +0100

    Set db to test

---
+++ app/tests/test_main.py
@@ -0,0 +1,28 @@
+import asyncio
+
+import pytest
+from httpx import AsyncClient
+
+from app.db.config import Base, test_async_session, test_engine
+
+from ..main import app, get_session
+
+
+async def override_get_session():
+    try:
+        db = test_async_session()
+        yield db
+    finally:
+        await db.close()
+
+
+async def init_models():
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(init_models())
+
+app.dependency_overrides[get_session] = override_get_session
+
+
